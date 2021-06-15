2193efd2b82be2812ebad45b3859c38bd2e1af59
Samuel Goldfarb <samuelghau@gmail.com>
2021-06-15 19:47:01 -0400

    chore: update readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # fantastic-barnacle
 Attempt to visualize effectiveness of confinement in Chile in slowing down covid-19 transmission
+
+## Development setup
+
+### Requirements
+
+* `Python 3.x`
+
+### Virtual environment
+
+Before starting, it is recommended that you use a virtual environment.
+
+Create virtual environment:
+
+`python -m venv env`
+
+Activate virtual environment:
+
+`source env/bin/activate`
+
+When you are finished, deactivate virtual environment:
+
+`deactivate`
+
+### Install libraries
+
+Install required libraries:
+
+`pip install -r requirements.txt`
+
+### Run
+
+`python main.py`
--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import requests
 import os
 
-active_cases_url = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto19/CasosActivosPorComuna.csv"
-phases_url = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto74/paso_a_paso.csv"
 
-active_cases_file_name = "active_cases.csv"
-phases_file_name = "phases.csv"
-
-tmp_folder_name = "tmp"
-
-active_cases_file_path = os.path.join(tmp_folder_name, active_cases_file_name)
-phases_file_path = os.path.join(tmp_folder_name, phases_file_name)
 
 def ensure_folder_exists(foldername):
 	try:
 	    # Create tmp folder
 	    os.mkdir(foldername)
-	    print("Directory " , foldername ,  " Created ") 
+	    print("Directory created: " + foldername) 
 	except FileExistsError:
-	    print("Directory " , foldername ,  " already exists")
+	    pass
 
 def download_and_save(url, filename):
 	print("Downloading " + url)
@@ -28,7 +19,19 @@ def download_and_save(url, filename):
 	        file.write(chunk)
 
 def get_data():
+	tmp_folder_name = "tmp"
+
 	ensure_folder_exists(tmp_folder_name)
+
+	active_cases_url = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto19/CasosActivosPorComuna.csv"
+	phases_url = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto74/paso_a_paso.csv"
+
+	active_cases_file_name = "active_cases.csv"
+	phases_file_name = "phases.csv"
+
+	active_cases_file_path = os.path.join(tmp_folder_name, active_cases_file_name)
+	phases_file_path = os.path.join(tmp_folder_name, phases_file_name)
+
 	download_and_save(active_cases_url, active_cases_file_path)
 	download_and_save(phases_url, phases_file_path)
 
